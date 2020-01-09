65fcf7af0bb3e77972718eaa6d0a8c2725e2dac9
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-09 22:14:47 +0100

    create my skills section

---
+++ js/effects.js
@@ -0,0 +1,17 @@
+//********************My skills********************
+const swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 10,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
