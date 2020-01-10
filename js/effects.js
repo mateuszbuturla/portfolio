9195383ad731351324f7b682d64e8c23f3afd52b
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-10 18:07:30 +0100

    animations with gsap

--- js/effects.js
+++ js/effects.js
@@ -4,6 +4,7 @@ const swiperSkills = new Swiper('.swiper-container', {
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
+    initialSlide: 4,
     coverflowEffect: {
         rotate: 10,
         stretch: 0,
@@ -14,4 +15,70 @@ const swiperSkills = new Swiper('.swiper-container', {
     pagination: {
         el: '.swiper-pagination',
     },
+});
+
+//********************Gsap********************
+TweenMax.from(".header__text h1", 1, {
+    delay: 0,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".header__text p", 1, {
+    delay: 0.5,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+document.addEventListener('scroll', () => {
+    if (window.scrollY >= document.querySelector('.about-me').offsetTop + 200) {
+        TweenMax.to(".about-me .section__name", 1, {
+            delay: 0,
+            opacity: 1,
+            y: -20,
+            ease: Expo.easeInOut
+        });
+        TweenMax.to(".about-me .avatar", 1, {
+            delay: 0.5,
+            opacity: 1,
+            y: -20,
+            ease: Expo.easeInOut
+        });
+        TweenMax.to(".about-me__desctiprion", 1, {
+            delay: 1,
+            opacity: 1,
+            y: -20,
+            ease: Expo.easeInOut
+        });
+    }
+    if (window.scrollY >= document.querySelector('.my-skills').offsetTop + 200) {
+        TweenMax.to(".my-skills .section__name", 1, {
+            delay: 0,
+            opacity: 1,
+            y: -20,
+            ease: Expo.easeInOut
+        });
+        TweenMax.to(".my-skills .swiper-container", 1, {
+            delay: 0.5,
+            opacity: 1,
+            y: -20,
+            ease: Expo.easeInOut
+        });
+    }
+    if (window.scrollY >= document.querySelector('.my-projects').offsetTop + 200) {
+        TweenMax.to(".my-projects .section__name", 1, {
+            delay: 0,
+            opacity: 1,
+            y: -20,
+            ease: Expo.easeInOut
+        });
+        TweenMax.to(".my-projects .section__flex-pr", 1, {
+            delay: 0.5,
+            opacity: 1,
+            y: -20,
+            ease: Expo.easeInOut
+        });
+    }
 });
