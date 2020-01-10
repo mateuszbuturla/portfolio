793e3bcd6ce76bca5cf689d8315dcedc9ffe2ef1
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-01-10 12:19:11 +0100

    parallax effect

--- js/effects.js
+++ js/effects.js
@@ -7,7 +7,7 @@ const swiper = new Swiper('.swiper-container', {
     coverflowEffect: {
         rotate: 10,
         stretch: 0,
-        depth: 100,
+        depth: 300,
         modifier: 1,
         slideShadows: false,
     },
