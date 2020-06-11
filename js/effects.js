1760621c4d65ab9578674fd1de035749a2054ec2
mateuszbuturla <2002mateuszbuturla@gmail.com>
2020-06-11 14:49:59 +0200

    create home page view for devices larger than 1000px

--- js/effects.js
+++ js/effects.js
@@ -1,6 +1,6 @@
 window.onload = () => {
 
-    if (window.screen.width < 900)
+    if (window.screen.width < 1000)
     {
         const t1 = new TimelineMax();
 
@@ -72,6 +72,83 @@ window.onload = () => {
 
         TweenMax.from("#button2", 3, {
             delay: 6,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        });
+    }
+    else
+    {
+        const t1 = new TimelineMax();
+
+        t1.from("#loadingRing", 4, {
+            delay: 0.4,
+            opacity: 0,
+            y:40,
+            ease: Expo.easeInOut
+        }).to("#loadingRing", 4, {
+            delay: 0.4,
+            x: 160,
+            ease: Expo.easeInOut
+        })
+
+        TweenMax.to("#loadingScreen", 4, {
+            delay: 6.4,
+            top: "-110%",
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.from("#logo", 3, {
+            delay: 7.4,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.from("#header", 3, {
+            delay: 8,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.from("#footer", 3, {
+            delay: 7.7,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.staggerFrom("#media ul li", 2, {
+            delay: 7.7,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        }, 0.1);
+
+        TweenMax.from("#line1", 3, {
+            delay: 8.2,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.from("#line2", 3, {
+            delay: 8.4,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.from("#button1", 3, {
+            delay: 7.6,
+            opacity: 0,
+            y: 20,
+            ease: Expo.easeInOut
+        });
+
+        TweenMax.from("#button2", 3, {
+            delay: 8,
             opacity: 0,
             y: 20,
             ease: Expo.easeInOut
