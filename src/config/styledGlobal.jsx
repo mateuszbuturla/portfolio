3912e52a8603a6faba3261ab9a8c65be8b657a99
mateuszbuturla <2002mateuszbuturla@gmail.com>
2021-05-29 21:22:59 +0200

    add colors to theme and import font

--- src/config/styledGlobal.jsx
+++ src/config/styledGlobal.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import NukuNukuFont from '../assets/NUKU1.TTF';
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Nuku Nuku';
+    src: local('Nuku Nuku'), local('NukuNuku'),
+    url(${NukuNukuFont}) format('ttf');
+    font-weight: 300;
+    font-style: normal;
+  }
+
   * {
     margin: 0;
     padding: 0;
     text-decoration: none;
     outline: none;
+    font-family: 'Nuku Nuku';
   }
   body {
     overflow-x: hidden;
   }
 `;
 
-const theme = {};
+const theme = {
+  colors: {
+    bg: '#060607',
+    fontFirst: '#FFFFFF',
+    fontSecond: '#7D7D7D',
+    fontThird: '#141414',
+  },
+};
 
 const Theme = ({ children }) => (
   <ThemeProvider theme={theme}>
